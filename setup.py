0d86b5e7c46111c27ee9b01ce02e5978c2448d9d
gbayarri <genis.bayarri@irbbarcelona.org>
2020-12-22 16:26:47 +0100

    Read the docs bug fixes

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
     packages=setuptools.find_packages(exclude=['docs', 'test']),
     install_requires=['biobb_common==3.5.1'],
     extras_require={ 
-     "sphinx": ["sphinx<=2"], 
+     "sphinx": ["sphinx<2"], 
     },
     python_requires='==3.7.*',
     classifiers=(
