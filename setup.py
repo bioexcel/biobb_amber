b77ab83469a0b6bcac7e00d0d2f16ccb074b3d35
gbayarri <genis.bayarri@irbbarcelona.org>
2021-10-14 13:13:35 +0200

    Fixing RTD

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
         "Bioexcel": "https://bioexcel.eu/"
     },
     packages=setuptools.find_packages(exclude=['docs', 'test']),
-    install_requires=['biobb_common==3.6.0'],
+    install_requires=['biobb_common==3.7.0'],
     python_requires='==3.7.*',
     classifiers=(
         "Development Status :: 3 - Alpha",
