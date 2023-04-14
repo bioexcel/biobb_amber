ad4b4f3ee3f7d383129591af2bfd6c85c977ab33
gbayarri <genis.bayarri@irbbarcelona.org>
2023-04-14 16:04:37 +0200

    Fixing testing & setup

--- setup.py
+++ setup.py
@@ -43,6 +43,8 @@ setuptools.setup(
     classifiers=(
         "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: MacOS :: MacOS X",
         "Operating System :: POSIX",
