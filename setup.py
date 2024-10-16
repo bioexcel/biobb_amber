29177b43ef80aa9abf6582d771eb791630ef6a71
Pau Andrio <andriopau@gmail.com>
2024-10-16 11:33:19 +0200

    [CI/CD](setup): Limiting to Python 3.9 and 3.10 to be compatible with Ambertools 22.5

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="biobb_amber",
-    version="4.2.0",
+    version="5.0.0",
     author="Biobb developers",
     author_email="adam.hospital@irbbarcelona.org",
     description="Biobb_amber is a BioBB category for AMBER MD package.",
@@ -19,8 +19,8 @@ setuptools.setup(
     },
     packages=setuptools.find_packages(exclude=['docs', 'test']),
     package_data={'biobb_amber': ['py.typed']},
-    install_requires=['biobb_common==4.2.0'],
-    python_requires='>=3.8',
+    install_requires=['biobb_common==5.0.0'],
+    python_requires='>=3.8, <3.11',
     entry_points={
         "console_scripts": [
             "amber_to_pdb = biobb_amber.ambpdb.amber_to_pdb:main",
